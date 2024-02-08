83b028f6f797c86b3dadf24c021fe77ee77fd891
VladimirImir <mimimvvv@gmail.com>
2024-02-08 13:21:35 +0200

    Hibernate. 5. - Entity.

--- src/main/java/com/dev/HibernateRunner.java
+++ src/main/java/com/dev/HibernateRunner.java
@@ -1,12 +1,15 @@
 package com.dev;
 
+import com.dev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingDeque;
 
 public class HibernateRunner {
@@ -18,11 +21,24 @@ public class HibernateRunner {
                 .getConnection("db.url", "db.username", "db.password");*/
 
         var configuration = new Configuration();
+        //configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        //configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (var sessionFactory = configuration.buildSessionFactory();
              var session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            var user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 11))
+                    .age(20)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
