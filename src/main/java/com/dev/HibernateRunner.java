dfafff745998cdabddbcfac85c0738097299f4d0
VladimirImir <mimimvvv@gmail.com>
2024-02-14 14:51:31 +0200

    Hibernate. 14. - Entity lifecycle.

--- src/main/java/com/dev/HibernateRunner.java
+++ src/main/java/com/dev/HibernateRunner.java
@@ -4,6 +4,7 @@ import com.dev.converter.BirthdateConverter;
 import com.dev.entity.Birthdate;
 import com.dev.entity.Role;
 import com.dev.entity.User;
+import com.dev.util.HibernateUtil;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -15,25 +16,39 @@ import java.time.LocalDate;
 public class HibernateRunner {
 
     public static void main(String[] args) throws SQLException {
-//        BlockingDeque<Connection> pool = null;
-//        Connection connection = pool.take();
-//        SessionFactory
 
-//        Connection connection = DriverManager
-//                .getConnection("db.url", "db.username", "db.password");
-//        Session
-        Configuration configuration = new Configuration();
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-//        configuration.addAnnotatedClass(User.class);
-        configuration.addAttributeConverter(new BirthdateConverter());
-        configuration.registerTypeOverride(new JsonBinaryType());
-        configuration.configure();
+        var user = User.builder()
+                .username("ivan1@gmail.com")
+                .lastname("Ivanov")
+                .firstname("Ivan")
+                .build();
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            try (var session1 = sessionFactory.openSession()) {
+                session1.beginTransaction();
+
+                session1.saveOrUpdate(user);
+
+                session1.getTransaction().commit();
+            }
+            try (var session2 = sessionFactory.openSession()) {
+                session2.beginTransaction();
+
+                user.setFirstname("Petr");
+                //session2.delete(user);
+
+                Object mergedUser = session2.merge(user);
+                //session2.refresh(user);
+
+                session2.getTransaction().commit();
+            }
+        }
+
+        /*try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
-            /*User user = User.builder()
+            *//*User user = User.builder()
                     .username("ivan1@gmail.com")
                     .firstname("Ivan")
                     .lastname("Ivanov")
@@ -46,19 +61,19 @@ public class HibernateRunner {
                     .birthDate(new Birthdate(LocalDate.of(2000, 1, 19)))
                     .role(Role.ADMIN)
                     .build();
-            session.save(user);*/
+            session.save(user);*//*
             var user = session.get(User.class, "ivan1@gmail.com");
             user.setLastname("Petrov2");
             session.flush();
 
             System.out.println(session.isDirty());
 
-            /*session.evict(user1);
+            *//*session.evict(user1);
             session.clear();
-            session.close();*/
+            session.close();*//*
 
             session.getTransaction().commit();
-        }
+        }*/
     }
 
 
