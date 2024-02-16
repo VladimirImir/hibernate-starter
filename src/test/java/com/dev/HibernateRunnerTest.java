ff23c67371bcd5525c20a2391601dc735166c9a0
VladimirImir <mimimvvv@gmail.com>
2024-02-16 20:03:24 +0200

    Hibernate. 27. - OneToMany.

--- src/test/java/com/dev/HibernateRunnerTest.java
+++ src/test/java/com/dev/HibernateRunnerTest.java
@@ -1,6 +1,11 @@
 package com.dev;
 
+import com.dev.entity.Company;
 import com.dev.entity.User;
+import com.dev.util.HibernateUtil;
+import lombok.Cleanup;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.Column;
@@ -19,6 +24,18 @@ import static java.util.stream.Collectors.joining;
 
 
 class HibernateRunnerTest {
+
+    @Test
+    void oneToMany() {
+        @Cleanup var sessionFactory = HibernateUtil.buildSessionFactory();
+        @Cleanup var session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        var company = session.get(Company.class, 1);
+        System.out.println("");
+
+        session.getTransaction().commit();
+    }
 
     /*@Test
     void checkGetReflectionApi() throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchFieldException {
